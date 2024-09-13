c6d24b39dd3300b135e39d6ed4dc1668ff5eeb7b
Matjaz Prtenjak <matjaz@matjazev.net>
2024-09-13 18:06:40 +0200

    Setting language in REST calls

--- GoodDaySPA/src/app/auth.interceptor.ts
+++ GoodDaySPA/src/app/auth.interceptor.ts
@@ -2,24 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '@app/auth.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private translate: TranslateService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    var currentLang = this.translate.currentLang || 'si';
+    currentLang = currentLang === 'sl' ? 'si' : currentLang;
+
+    var cloned = req.clone({
+      headers: req.headers
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .set('Accept-Language', `${currentLang},en;q=0.5`)
+    });
+
+    console.log('AuthInterceptor: intercept I', cloned);
     const token = this.authService.getToken();
     if (token) {
-      const cloned = req.clone({
-        headers: req.headers
-          .set('Authorization', `Bearer ${token}`)
-          .set('Content-Type', 'application/json')
-          .set('Accept', 'application/json')
-          .set('Accept-Language', 'si, si-SL,en;q=0.5')
+      cloned = cloned.clone({
+        headers: cloned.headers.set('Authorization', `Bearer ${token}`)
       });
-      return next.handle(cloned);
-    } else {
-      return next.handle(req);
-    }
+    } 
+
+    console.log('AuthInterceptor: intercept II', cloned);
+    return next.handle(cloned);
   }
 }
